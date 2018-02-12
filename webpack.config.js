b3997b20b034dd82bedd67705a5bdd8389b45396
marcpre <marcus.presich@gmail.com>
2018-02-12 05:33:34 +0000

    changing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './app/javascripts/app.js',
+  entry: [
+    'webpack-dev-server/client?https://0.0.0.0:8080',
+    'webpack/hot/dev-server',
+    './app/javascripts/app.js'
+  ],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'app.js'
@@ -16,35 +20,35 @@ module.exports = {
   ],
   module: {
     rules: [{
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    },
-    {
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-    },
-    {
-      test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      use: 'url-loader?limit=10000'
-    },
-    {
-      test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-      use: 'file-loader'
-    }
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader'
+      }
     ],
     loaders: [{
-      test: /\.json$/,
-      use: 'json-loader'
-    },
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader',
-      query: {
+        test: /\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
           presets: ['es2015'],
           plugins: ['transform-runtime']
         }
-    }
+      }
     ]
   }
 }
